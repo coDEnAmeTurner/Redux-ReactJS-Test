0ee13018ba4bb38b2613e1dcee837ac3210ba706
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2025-01-19 16:51:43 +0700

    Performace Optimization, CreateSelector, CreateEntityAdapter

--- redux_reactjs_test/src/components/Header.js
+++ redux_reactjs_test/src/components/Header.js
@@ -1,19 +1,44 @@
 import { Link } from "react-router-dom";
 
-import React from 'react'
+import React from "react";
+
+import { increaseCount, getCount } from "../features/posts/postsSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const count = useSelector(getCount);
+
   return (
     <header>
-        <h1>Redux</h1>
-        <nav>
-            <ul style={{display: "flex", justifyContent: "space-around", listStyle: "none"}}>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="post">Post</Link></li>
-            </ul>
-        </nav>
+      <h1>Redux</h1>
+      <nav style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            listStyle: "none",
+          }}>
+        <ul
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            listStyle: "none",
+          }}
+        >
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="post">Post</Link>
+          </li>
+          <li>
+            <Link to="user">Users</Link>
+          </li>
+        </ul>
+        <button style={{display: "block"}} onClick={() => dispatch(increaseCount())}>{count}</button>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

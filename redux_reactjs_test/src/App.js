f8b668e986d06b886cff584eca21b294d85d068a
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2025-01-24 16:37:49 +0700

    RTK Intro finished

--- redux_reactjs_test/src/App.js
+++ redux_reactjs_test/src/App.js
@@ -9,33 +9,39 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import EditPostForm from './components/EditPostForm';
 import UsersList from './components/UsersList';
 import UserPage from './components/UserPage';
+import ToDoList from './features/todos/ToDoList';
+
 
 function App() {
   return (
-    <Routes>
-      {/* {this means / redirects to Layout component} */}
-      <Route path='/' element={<Layout/>}>
-      {/*index: the corresponding element is what you see immediately after redirect to / */}
-        <Route index element={<PostsList/>}/>
-        {/*This route means /post/ */}
-        <Route path='post'>
-          <Route index element={<AddPostsForm/>}/>
-        {/*This route means /post/postId, basically path param */}
-          <Route path=':postId' element={<SinglePostPage/>}/>
-          <Route path='edit/:postId' element={<EditPostForm/>}/>
-        </Route>
+    //code before RTK chapter
+    // <Routes>
+    //   {/* {this means / redirects to Layout component} */}
+    //   <Route path='/' element={<Layout/>}>
+    //   {/*index: the corresponding element is what you see immediately after redirect to / */}
+    //     <Route index element={<PostsList/>}/>
+    //     {/*This route means /post/ */}
+    //     <Route path='post'>
+    //       <Route index element={<AddPostsForm/>}/>
+    //     {/*This route means /post/postId, basically path param */}
+    //       <Route path=':postId' element={<SinglePostPage/>}/>
+    //       <Route path='edit/:postId' element={<EditPostForm/>}/>
+    //     </Route>
 
-        <Route path="user">
-          <Route index element={<UsersList/>}/>
-          <Route path=':userId' element={<UserPage/>}/>
-        </Route>
+    //     <Route path="user">
+    //       <Route index element={<UsersList/>}/>
+    //       <Route path=':userId' element={<UserPage/>}/>
+    //     </Route>
 
-        {/* this is in case no route is matched
-        replace: replace all non existing routes in the history with the / route
-         */}
-        <Route path='*' element={<Navigate to="/" replace/>}></Route>
-      </Route>
-    </Routes>
+    //     {/* this is in case no route is matched
+    //     replace: replace all non existing routes in the history with the / route
+    //      */}
+    //     <Route path='*' element={<Navigate to="/" replace/>}></Route>
+    //   </Route>
+    // </Routes>
+
+    //code after rtk chapter
+    <ToDoList/>
   );
 }
 
--- redux_reactjs_test/src/index.js
+++ redux_reactjs_test/src/index.js
@@ -1,31 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { store } from './store';
-import {Provider} from 'react-redux';
-import { fetchUsers } from './features/users/usersSlice';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { fetchPosts } from './features/posts/postsSlice';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlice";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { fetchPosts } from "./features/posts/postsSlice";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./features/api/apiSlice";
 
 store.dispatch(fetchUsers());
 
-//when F5, this file renders before all do, and it only fetches the users, not the posts, so 
+//when F5, this file renders before all do, and it only fetches the users, not the posts, so
 //if F5 without calling fetchPosts, there will be "Page not found" screen on display
 store.dispatch(fetchPosts());
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //StrictMode can cause your component logic to go insane
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          {/*this means /something_here will redirect to App component*/}
-          <Route path='/*' element={<App />}></Route>
-        </Routes>
-      </Router>
-    </Provider>
+  //below is the code before RTX Query chapter
+  // <Provider store={store}>
+  //   <Router>
+  //     <Routes>
+  //       {/*this means /something_here will redirect to App component*/}
+  //       <Route path='/*' element={<App />}></Route>
+  //     </Routes>
+  //   </Router>
+  // </Provider>
+
+  //this is the code for RTX Query chapter
+  <ApiProvider api={apiSlice}>
+    <App />
+  </ApiProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
